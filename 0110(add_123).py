fb54e78cb3a089603832805d2110f52557d4155a
Sangmin Lee <sm32289@naver.com>
2023-01-10 13:30:58 +0900

    0110(DP3)

---
+++ 0110(add_123).py
@@ -0,0 +1,14 @@
+def recursive(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return recursive(n-2) + recursive(n-1) + recursive(n-3)
+
+N = int(input())
+for i in range(N):
+    a = int(input())
+    print(recursive(a))
