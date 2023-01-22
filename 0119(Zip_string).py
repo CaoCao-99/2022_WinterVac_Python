c468b451cf3a39311ffd02ce92b35f4c5849b6f1
Sangmin Lee <sm32289@naver.com>
2023-01-22 13:57:12 +0900

    0122

--- 0119(Zip_string).py
+++ 0119(Zip_string).py
@@ -1,23 +1,27 @@
 data = input()
 length = len(data)//2
 once = True
-answer = [0] * (len(data)//2 +1)
+answer = [0] * (length+1)
 print(answer)
-for i in range(1,length):           #구간을 하나씩 늘려가며 설정
-    for j in range(0,len(data),i):  #늘어가는 구간 만큼 반복문 증가
-        print(j)
-        now_data = data[j:j+i]
-        answer[i] += i
-        once = True
-        next_data = data[j+i: j+2*i]
+for i in range(1,length+1):           #구간을 하나씩 늘려가며 설정
+    now_data = data[:i]
+    once = True
+    for j in range(i,len(data),i):  #늘어가는 구간 만큼 반복문 증가
+        next_data = data[j: j+i]
         if now_data == next_data:
             if once:
-                answer[i]+=1
-                once = False
+                once=False
+        else:
+            if once:
+                answer[i] += len(now_data)
             else:
-                continue
-
+                answer[i] += 1 + len(now_data)
+            now_data = next_data
+            once=True
+    if once:
+        answer[i] += len(now_data)
+    else:
+        answer[i] += len(now_data)+1
 answer[0] = 1000
-answer[length] = 1000
 print(answer)
 print(min(answer))
--- asd.py
+++
@@ -1,6 +0,0 @@
-global i
-for i in range(10):
-    print(i)
-    for j in range(10):
-        if j == 3:
-            i = 10
